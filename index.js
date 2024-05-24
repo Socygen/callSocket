95c66906121321b9bcecda54dd80e9b098ea784a
Ashutosh <socygen@gmail.com>
2024-05-24 14:44:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io'); 
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+const socketIdToUserId = new Map();
+
+io.on('connection', (socket) => {
+    console.log(`Socket connected: ${socket.id}`);
+
+    socket.on('offer', (data) => {
+        socket.broadcast.emit('offer', data);
+    });
+
+    socket.on('answer', (data) => {
+        socket.broadcast.emit('answer', data);
+    });
+
+    socket.on('candidate', (data) => {
+        socket.broadcast.emit('candidate', data);
+    });
+
+    socket.on('user_online', ({ userId }) => {
+        socketIdToUserId.set(socket.id, userId);
+        console.log(`User ${userId} is online`);
+    });
+
+    socket.on('disconnect', () => {
+        const userId = socketIdToUserId.get(socket.id);
+        socketIdToUserId.delete(socket.id);
+        console.log(`User ${userId} disconnected`);
+    });
+});
+
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
